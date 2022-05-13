8a706e5521399d397f8c01fe83f485b9c6289974
itzzbas <74045534+itzzbas@users.noreply.github.com>
2022-05-13 10:25:14 +0200

    Eerste versie auth0 toegevoegd

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createAuth0 } from '@auth0/auth0-vue';
+
+const app = createApp(App);
 
 Vue.config.productionTip = false
+
+app.use(
+    createAuth0({
+        domain: "dev-nghy1l73.us.auth0.com",
+        client_id: "lMp8Vzq9azOKzklvdBQeZRwNVu7J8suh",
+        redirect_uri: window.location.origin
+    })
+);
 
 new Vue({
   router,
