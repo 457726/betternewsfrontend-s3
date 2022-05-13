166869a36f539b756af9925f92c03d923704a826
itzzbas <74045534+itzzbas@users.noreply.github.com>
2022-05-13 11:01:55 +0200

    Auth0 functionaliteit toegevoegd, en voor zo ver werkend gekregen.
    Volgende stap: de data ophalen dmv de juiste api's icm axios

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Callback from '@/views/Callback.vue'
 
 Vue.use(VueRouter)
 
@@ -17,7 +18,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    },
+    {
+        path: '/callback',
+        name: 'callback',
+        component: Callback
+    }
 ]
 
 const router = new VueRouter({
@@ -25,5 +31,16 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+//// very basic "setup" of a global guard
+//router.beforeEach((to, from, next) => {
+//    if (to.name == 'callback') { // check if "to"-route is "callback" and allow access
+//        next()
+//    } else if (router.app.$auth.isAuthenticated()) { // if authenticated allow access
+//        next()
+//    } else { // trigger auth0 login
+//        router.app.$auth.login()
+//    }
+//})
 
 export default router
---
+++ src/views/Callback.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="callback">Callback</div>
+</template>
+
+<script>
+export default {
+  name: 'callbackView',
+  mounted() {
+    this.$auth.handleAuthentication().then(() => {
+      this.$router.push({ name: 'home' })
+    })
+  }
+}
+</script>
